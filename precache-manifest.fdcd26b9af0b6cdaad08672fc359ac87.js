3afac193efdd38a8e5450ff450d8699e75a0d1c5
Lipich <li98pen15sa09sha@gmail.com>
2019-12-25 18:24:35 +0300

    Updates

---
+++ precache-manifest.fdcd26b9af0b6cdaad08672fc359ac87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8705d4a4013c3f7dacf9df76165248f1",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "0f7a4e27627dbef76244",
+    "url": "/portfolio-web/static/css/main.c0678a11.chunk.css"
+  },
+  {
+    "revision": "eca80face5de6fdf87e0",
+    "url": "/portfolio-web/static/js/2.8beb46d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-web/static/js/2.8beb46d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f7a4e27627dbef76244",
+    "url": "/portfolio-web/static/js/main.c4747c33.chunk.js"
+  },
+  {
+    "revision": "c37106b24ae9a988c31f",
+    "url": "/portfolio-web/static/js/runtime-main.1cc4c1da.js"
+  }
+]);
