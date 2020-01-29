e6e6145d7a890933b273241e3e16faefb9dae859
Lipich <li98pen15sa09sha@gmail.com>
2020-01-29 17:02:59 +0300

    Added background in main block

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ function Footer() {
             <div className={styles.insideBorder}>
                 <span className={styles.text}>Alexander Viktorovich</span>
                 <div className={styles.containerFooter}>
-                  <a href="https://vk.com/alexander_lipen"  className={styles.link}>1</a>
-                  <a href="https://www.instagram.com/lip_ok/" className={styles.link}>2</a>
-                  <a href="#" className={styles.link}>3</a>
-                  <a href="#" className={styles.link}>4</a>
+                  <a href="https://vk.com/alexander_lipen"  className={styles.link}></a>
+                  <a href="https://www.instagram.com/lip_ok/" className={styles.link}></a>
+                  <a href="#" className={styles.link}></a>
+                  <a href="#" className={styles.link}></a>
                 </div>
               <span className={styles.text}>All rights reserved ©</span>
             </div>
