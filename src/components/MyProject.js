123461d3f27820d76efd4e4541fecd62bff67fa0
Lipich <li98pen15sa09sha@gmail.com>
2020-02-24 01:23:34 +0300

    86% portfolio completed

--- src/components/MyProject.js
+++ src/components/MyProject.js
@@ -13,9 +13,9 @@ function MyProject() {
                     <span className={styles.text}>My projects</span>
                 </div>
                 <div className={styles.blocks}>
-                    <Project img={"https://2.downloader.disk.yandex.by/preview/fe2442f81e49d4def9460e05b954dfc0955dd6f33a956f51361bc1f358364ca0/inf/lnmS5G1rYAh3Cr2FNdcTwEEqmUurfLn5zbIQT4VoeWcLoXBYGouN23T-d0_V5uiXoHASlLAu6TrRUm9FzzE6rQ%3D%3D?uid=348254188&filename=Screenshot_2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=348254188&tknv=v2&size=1903x937"} title='Dog game' link='https://github.com/Lip-ok/SN-project'/>
-                    <Project img={"https://3.downloader.disk.yandex.by/preview/3587327d1a70966229338623306b6105e35f0a0149d25a007306ce60029796b4/inf/m6Ltq1kwdRFC-wJcmaruEEEqmUurfLn5zbIQT4VoeWeQv09CUDI-iwy89-rSx2lq90oJszaCP8Dbi7Jo9flRqg%3D%3D?uid=348254188&filename=Screenshot_3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=348254188&tknv=v2&size=1903x937"} title='Social network' link='https://github.com/Lip-ok/SN-project'/>
-                    <Project img={"https://2.downloader.disk.yandex.by/preview/ec371724a1f0e18d03fcebe0de789a340d01998ca83e34813d07de08362d4ed6/inf/qyZOduxJyOGZs37VvfZqMEEqmUurfLn5zbIQT4VoeWf76LUh31_R4FkFe09pSDASp-ED9XPu6rbny-k01Tqcag%3D%3D?uid=348254188&filename=Screenshot_1.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=348254188&tknv=v2&size=1903x937"} title='ToDoList' link='https://github.com/Lip-ok/todolist'/>
+                    <Project img={"https://lh3.googleusercontent.com/proxy/R7n5atY5UXdXo1bfA9WdqhGnDbU7qpiPSOj6_fgcyf1LquGsInBBxqc_4XOT0RZHdJFZgz8VaiVNu3K2kCwjeEg78qQEywoYTaKPL5-4i_XIodoAOK0htn541Jgq1hXfAcPOmZZsHKGqewb9QbIwhT2yqROoek4"} title='Dog game' link='https://github.com/Lip-ok/SN-project'/>
+                    <Project img={"https://techrocks.ru/wp-content/uploads/2018/01/use-case-2-css.png"} title='Social network' link='https://github.com/Lip-ok/SN-project'/>
+                    <Project img={"https://vignette.wikia.nocookie.net/science/images/b/b7/Html-source-code.png/revision/latest/scale-to-width-down/340?cb=20130801094623&path-prefix=ru"} title='ToDoList' link='https://github.com/Lip-ok/todolist'/>
                 </div>
             </div>
         </div>
