1e2755d64fd85e15cbc1b0cc7a6e7466cac59732
Lipich <li98pen15sa09sha@gmail.com>
2020-02-24 00:39:08 +0300

    Updates

---
+++ precache-manifest.cdb51f9f141d329a55609accdfcc0cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9003fe3f9df079e0ef01ffe2c1971f78",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "2bddc815f5cc23102de7",
+    "url": "/portfolio-web/static/css/main.ad775a1c.chunk.css"
+  },
+  {
+    "revision": "53343760b4fcb3a8d606",
+    "url": "/portfolio-web/static/js/2.f54b100b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio-web/static/js/2.f54b100b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bddc815f5cc23102de7",
+    "url": "/portfolio-web/static/js/main.f4ab78f4.chunk.js"
+  },
+  {
+    "revision": "bdf30d9937fde82dbb5e",
+    "url": "/portfolio-web/static/js/runtime-main.a4931acf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-web/precache-manifest.941f1c01a21cfcf596f4b858ffa2c58c.js"
+  "/portfolio-web/precache-manifest.cdb51f9f141d329a55609accdfcc0cd7.js"
 );
 
 self.addEventListener('message', (event) => {
