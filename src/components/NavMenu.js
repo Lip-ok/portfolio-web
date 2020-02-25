2f3e3addf5529e254937cc98538f456a072232d7
Lipich <li98pen15sa09sha@gmail.com>
2020-02-25 15:29:37 +0300

    91.1% portfolio completed

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -9,22 +9,22 @@ function NavMenu() {
             <a ><Link to='main' spy={true}
                                                activeClass={styles.active}
                                                smooth={true}
-                                               // offset={-70}
+                                               offset={1}
                                                duration={500} className={styles.button}>Main</Link></a>
             <a ><Link to='skills' spy={true}
                                                activeClass={styles.active}
                                                smooth={true}
-                                               // offset={-70}
+                                               offset={1}
                                                duration={500} className={styles.button}>Skills</Link></a>
             <a ><Link to='projects' spy={true}
                                                activeClass={styles.active}
                                                smooth={true}
-                                               // offset={-70}
+                                               offset={1}
                                                duration={500} className={styles.button}>Projects</Link></a>
             <a ><Link to='contactsId' spy={true}
                                                activeClass={styles.active}
                                                smooth={true}
-                                               // offset={-70}
+                                               offset={1}
                                                duration={500} className={styles.button}>Contacts</Link></a>
 
         </div>
